9c85ac9456e20a276efa4bbe8e0ae4e953ee2b5d
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-08-20 18:23:23 -0700

    working on spotify search functions

--- liri.js
+++ liri.js
@@ -27,3 +27,33 @@ var spotify = new Spotify(keys.spotify);
 var getArtistNames = function(artist) {
     return artist.name;
 };
+
+// Function for running a Spotify search - Command is spotify-this-song
+var getSpotify = function(songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }
+
+    spotify.search({
+            type: "track",
+            query: userCommand
+        },
+        function(err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
